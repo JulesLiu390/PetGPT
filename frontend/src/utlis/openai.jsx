ddf0b22ed3159bf0c0f8657de1a77b87664a81b0
JulesLiu390 <liunxryojeen@gmail.com>
2025-03-22 02:37:10 -0400

    finish simple demo

--- frontend/src/utlis/openai.jsx
+++ frontend/src/utlis/openai.jsx
@@ -1,7 +1,6 @@
 import OpenAI from "openai/index.mjs";
 const openai = new OpenAI({
-  apiKey: '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',
-  dangerouslyAllowBrowser: true, 
+  apiKey: 
 });
 
 export const callOpenAI = async (messages) => {
